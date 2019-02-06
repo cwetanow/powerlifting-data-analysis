d2712acbaad68ed7fa52f4d7053b98c1bbfc4d0d
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-02-06 12:15:51 +0200

    Add console app to test parsing

---
+++ src/PowerliftingPredictor.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using PowerliftingPredictor.Core.Utils;
+
+namespace PowerliftingPredictor.CLI
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var helper = new DataHelper();
+
+			var (results, meets) = helper.LoadData(@"../../../Data/openpowerlifting.csv");
+		}
+	}
+}
