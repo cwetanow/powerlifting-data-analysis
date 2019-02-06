bcb01662f045eec9f3248333ee3ece09a319e01f
Ivan <madritistaaa@gmail.com>
2019-02-06 19:22:16 +0200

    Return percentile

--- src/PowerliftingPredictor.Core/PredictionService.cs
+++ src/PowerliftingPredictor.Core/PredictionService.cs
@@ -7,27 +7,39 @@ namespace PowerliftingPredictor.Core
 {
 	public class PredictionService
 	{
-		public string PredictPlace(MeetResult expectedResult, IEnumerable<MeetResult> dataset)
+		public (string place, double percentile) PredictPlace(MeetResult expectedResult, IEnumerable<MeetResult> dataset)
 		{
 			var sameDivisionResults = dataset
-				.Where(result =>// result.Division == expectedResult.Division &&
-				result.Equipment == expectedResult.Equipment &&
-				result.WeightClass == expectedResult.WeightClass &&
-				result.Sex == expectedResult.Sex)
+				.Where(result => result.Division == expectedResult.Division &&
+					result.Equipment == expectedResult.Equipment &&
+					result.WeightClass == expectedResult.WeightClass &&
+					result.Sex == expectedResult.Sex &&
+					(!expectedResult.Age.HasValue || result.Age.HasValue) &&
+					result.Total.HasValue && result.Bodyweight.HasValue &&
+					AreSameMovements(expectedResult, result))
 				.ToList();
+
 
 			var k = (int)Math.Sqrt(sameDivisionResults.Count);
 
-			var nearestNeighbours = GetNearestNeighbours(expectedResult, k, sameDivisionResults);
+			var nearestNeighbors = GetNearestNeighbours(expectedResult, k, sameDivisionResults);
 
-			var place = nearestNeighbours
+			var place = nearestNeighbors
 				.GroupBy(n => n.Place)
 				.Select(group => new { classification = group.Key, distanceSum = group.Select(x => 1 / GetDistance(expectedResult, x)).Sum() })
 				.OrderByDescending(x => x.distanceSum)
 				.Select(x => x.classification)
 				.FirstOrDefault();
 
-			return place;
+			sameDivisionResults.Add(expectedResult);
+
+			var percentile = (sameDivisionResults
+				.OrderByDescending(r => r.Total)
+				.ThenBy(r => r.Bodyweight)
+				.ToList()
+				.IndexOf(expectedResult)) / (double)sameDivisionResults.Count;
+
+			return (place, percentile);
 		}
 
 		private ICollection<MeetResult> GetNearestNeighbours(MeetResult expectedResult, int k, IEnumerable<MeetResult> dataset)
@@ -43,12 +55,43 @@ namespace PowerliftingPredictor.Core
 		private double GetDistance(MeetResult expectedResult, MeetResult result)
 		{
 			var distance = Math.Pow(expectedResult.Total.Value - result.Total.Value, 2);
-			distance += Math.Pow(expectedResult.Age.Value - result.Age.Value, 2);
 			distance += Math.Pow(expectedResult.Bodyweight.Value - result.Bodyweight.Value, 2);
+
+			if (expectedResult.Age.HasValue && result.Age.HasValue)
+			{
+				distance += Math.Pow(expectedResult.Age.Value - result.Age.Value, 2);
+			}
 
 			distance = Math.Sqrt(distance);
 
 			return distance;
+		}
+
+		private bool AreSameMovements(MeetResult result, MeetResult otherResult)
+		{
+			// All movements
+			if (result.Squat.HasValue && otherResult.Squat.HasValue &&
+				result.Bench.HasValue && otherResult.Bench.HasValue &&
+				result.Deadlift.HasValue && otherResult.Deadlift.HasValue)
+			{
+				return true;
+			}
+
+			if ((result.Squat.HasValue && otherResult.Squat.HasValue && result.Bench.HasValue && otherResult.Bench.HasValue) ||
+				(result.Squat.HasValue && otherResult.Squat.HasValue && result.Deadlift.HasValue && otherResult.Deadlift.HasValue) ||
+				(result.Deadlift.HasValue && otherResult.Deadlift.HasValue && result.Bench.HasValue && otherResult.Bench.HasValue))
+			{
+				return true;
+			}
+
+			if ((result.Squat.HasValue && otherResult.Squat.HasValue) ||
+				(result.Deadlift.HasValue && otherResult.Deadlift.HasValue) ||
+				(result.Bench.HasValue && otherResult.Bench.HasValue))
+			{
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
