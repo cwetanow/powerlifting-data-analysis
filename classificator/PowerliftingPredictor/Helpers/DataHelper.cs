78afa0ce165191237ccfe78d41431a41694dcbcb
Ivan <madritistaaa@gmail.com>
2019-02-08 17:48:13 +0200

    Implement data reading

---
+++ classificator/PowerliftingPredictor/Helpers/DataHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+using PowerliftingPredictor.Models;
+
+namespace PowerliftingPredictor.Helpers
+{
+	public class DataHelper
+	{
+		public IEnumerable<MeetResult> LoadData(string filePath)
+		{
+			using (var reader = new CsvReader(new StreamReader(filePath), true))
+			{
+				var results = new List<MeetResult>();
+
+				while (reader.ReadNextRecord())
+				{
+					var result = MeetResult.Create(reader[0], reader[1], reader[2], reader[3], reader[4], reader[5], reader[6], reader[7], reader[9], reader[11], reader[13], reader[14], reader[15], reader[16]);
+
+					if (result.Total.HasValue && result.Bodyweight.HasValue && !string.IsNullOrEmpty(result.Division) && !string.IsNullOrEmpty(result.Equipment) && !string.IsNullOrEmpty(result.WeightClass))
+					{
+						results.Add(result);
+					}
+				}
+
+				return results;
+			}
+		}
+	}
+}
